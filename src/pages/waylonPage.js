67a1bd55687c2bd38d2780d84dca07a550bbeb1b
WaylonWeirather <wpweirather@uiowa.edu>
2022-04-18 12:32:30 -0500

    Update waylonPage.js

--- src/pages/waylonPage.js
+++ src/pages/waylonPage.js
@@ -7,7 +7,12 @@ const waylonPage = () => (
   <Layout>
     <Link to="/">Back to the home page</Link>
     <h1>Waylon Weirather</h1>
-    <p className="personalPageInfo"> I am a senior electrical engineering honors student with a focus in power systems at the University of Iowa. In the summer I work for Ameren UE, an energy provider for Illinois and Missouri. I work at the Keokuk Energy Center shown in the picture. I have overseen the upgrade of 3 exciter units, assisted with an upgrade to the control system for one generator, and assisted with the commissining of a newly rebuilt unit. Personally, I love to play baseball and coach young pitchers. Please reach out to me at wpweirather@uiowa.edu with any questions, or leave a comment below!</p>
+    <p className="personalPageInfo"> I am a senior electrical engineering honors student with a focus in power systems at the University of Iowa. 
+                                      In the summer I work for Ameren UE, an energy provider for Illinois and Missouri. I work at the Keokuk Energy 
+                                      Center shown in the picture. I have overseen the upgrade of 3 exciter units, assisted with an upgrade to the 
+                                      control system for one generator, and assisted with the commissining of a newly rebuilt unit. Personally, 
+                                      I love to play baseball and coach young pitchers. 
+                                      Please reach out to me at wpweirather@uiowa.edu with any questions, or leave a comment below!</p>
     <div className="image-container">
     <StaticImage
       src="../images/KeokukEnergyCenterNight.jpg"
