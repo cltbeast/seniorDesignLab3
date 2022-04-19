0ac93a57dedaf1b612dafbaff301bdc4860bd121
Collin Thornton <thorntoncollin1@gmail.com>
2022-04-19 14:10:53 -0500

    redoing Collin's header

--- src/pages/collinPage.js
+++ src/pages/collinPage.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 const collinPage = () => (
   <Layout>
     <Link to="/">Back to the home page</Link>
-    <h1>Collin Thornton COllin</h1>
+    <h1>Collin Thornton</h1>
     <p className="personalPageInfo"> Collin's bio</p>
     <div className="image-container">
     <StaticImage
