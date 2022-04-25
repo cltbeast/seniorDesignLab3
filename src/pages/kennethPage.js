b4615369c6f5a9377b48fafa982adcc66bc01be2
Horst, Kenneth A <kenneth-horst@uiowa.edu>
2022-04-25 11:38:25 -0500

    Update kennethPage.js

--- src/pages/kennethPage.js
+++ src/pages/kennethPage.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
-import Video from "../components/video"
 
 const kennethPage = () => (
   <Layout>
