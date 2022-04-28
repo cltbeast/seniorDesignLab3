d3bb2fbab9b906285ba28d7d1973e75fe9df8211
WaylonWeirather <wpweirather@uiowa.edu>
2022-04-28 17:10:14 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Layout from "../components/layout"
 const IndexPage = () => (
   <Layout>
     <link rel="stylesheet" href = "../components/layout.css"></link>
-    <h1 className = 'header'>Click the links below to learn more about us!</h1>
+    <h1 className = 'header'>Welcome to Team Top 3 && !(2||3)'s Home Page!</h1>
     <div className = 'linkRefs'>
     <p>
       <Link className = 'navBarLink' to="/teamIntro/">Team Introduction</Link>
@@ -17,10 +17,10 @@ const IndexPage = () => (
       <Link className = 'navBarLink' to="/waylonPage">Waylon Weirather</Link>
     </p>
     <p className="teamDescription">
-    Welcome to Team Top 3 && !(2||3)'s Home Page!
-    </p>
-    <p className="teamDescription">
-    Feel free to click around and get to know our team members
+    Feel free to click around and get to know the team behind the Pitcher's Plate (pictured below)<br></br>
+    The Pitcher's Plate is an affordable baseball pitch identifying system that uses high tech laser sensors
+    to call balls and strikes during baseball practice. Pitchers can now practice on their own with just a ball,
+    a phone, and the Pitcher's Plate. Please reach out to any of our members or leave a comment below!
     </p>
     </div>
     <div className = 'center-container'>
