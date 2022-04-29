7d34713e485f09de72a6f6cc8d994322c19da343
Collin Thornton <thorntoncollin1@gmail.com>
2022-04-29 10:22:33 -0500

    final

--- src/pages/collinPage.js
+++ src/pages/collinPage.js
@@ -18,7 +18,7 @@ const collinPage = () => (
     <div className="center-container">
     <StaticImage
       src="../images/collinCardinalsGame.jpeg"
-      width={300}
+      width={400}
       quality={90}
       alt="collin game"
     />
@@ -28,7 +28,7 @@ const collinPage = () => (
     
     <StaticImage
       src="../images/collinFishing.jpg"
-      width={300}
+      width={400}
       quality={90}
       alt="fishing"
     />
