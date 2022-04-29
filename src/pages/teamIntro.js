62285f3f6ecc150164d80dab0663b0396ea9c592
Collin Thornton <thorntoncollin1@gmail.com>
2022-04-28 21:42:47 -0500

    team intro page

--- src/pages/teamIntro.js
+++ src/pages/teamIntro.js
@@ -51,7 +51,7 @@ const teamIntroPage = () => (
       />
       </div>
       <div class = "column-list">
-        <p>Major:Computer Engineering and Electrical Engineering</p>
+        <p>Major: Computer Engineering and Electrical Engineering</p>
         <p>Skills: WireShark, ADM, Powershell, Python, C++</p>
         <p>Interests:Robotics, Cybersecurity</p>
         <p>Expected Graduation: May 2023</p>
