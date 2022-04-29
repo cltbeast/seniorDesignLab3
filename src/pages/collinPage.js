ec2eea07808add6acd00a1df5364eeaa640fd123
Collin Thornton <thorntoncollin1@gmail.com>
2022-04-28 21:15:48 -0500

    added collin bio

--- src/pages/collinPage.js
+++ src/pages/collinPage.js
@@ -11,7 +11,9 @@ const collinPage = () => (
     <Link to="/">Back to the home page</Link>
     <h1>Collin Thornton</h1>
     <p className="personalPageInfo"> Hello, I am a senior Computer Science and Engineering student at the University of Iowa
-    with a focus in Software Engineering.  </p>
+   with a focus in Software Engineering. I've worked at the Surplus store here at the University of Iowa as a Computer Technician,
+   restoring University hard drives and computers. In my free time I enjoy learning new things, hanging out with friends, watching sports, and playing video games.
+    Feel free to reach out to me via email at collin-thornton@uiowa.edu or by phone at 319-371-5407.  </p>
     
     <div class="row">
         <div class="column">
@@ -26,9 +28,8 @@ const collinPage = () => (
         </div>
         <div class="column">
           <StaticImage
-          src="../images/collinCardinalsGame.jpg"
+          src="../images/collinCardinalsGame.jpeg"
           width={300}
-          height={225}
           quality={90}
           alt="cardinals game"
           />
